99361dad3596e4bce4dd5467e1af878c6ef6af1e
KHallberg <hallberg.kristoffer@gmail.com>
2016-03-11 09:14:23 +0100

    testUpload

---
+++ TestUpload/src/Grupp_7_Test.java
@@ -0,0 +1,61 @@
+import java.awt.BorderLayout;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.ImageIcon;
+
+public class Grupp_7_Test extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Grupp_7_Test frame = new Grupp_7_Test();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Grupp_7_Test() 
+	{
+		setBackground(Color.GRAY);Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	double width = screenSize.getWidth();
+	double height = screenSize.getHeight();
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(0, 0, (int)width, (int)height);
+		this.setUndecorated(true);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel();
+		lblNewLabel_1.setIcon(new ImageIcon(Grupp_7_Test.class.getResource("/Wireframe_img/DriftInfo_Screen-1680-1050.jpg")));
+		lblNewLabel_1.setBounds(0, -100, (int)width, (int)height);
+		contentPane.add(lblNewLabel_1);
+	}
+}
