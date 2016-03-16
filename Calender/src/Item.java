73f78c23a539269e71cd4f7f8bb6d62e2a1ed87c
KHallberg <hallberg.kristoffer@gmail.com>
2016-03-16 13:00:37 +0100

    Adding to arryList

---
+++ Calender/src/Item.java
@@ -0,0 +1,33 @@
+
+public class Item {
+private String description;
+private String title;
+private String time;
+private String timeExact;
+
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getTime() {
+	return time;
+}
+public void setTime(String time) {
+	this.time = time;
+}
+public String getTimeExact() {
+	return timeExact;
+}
+public void setTimeExcact(String timeExact) {
+	this.timeExact = timeExact;
+}
+
+}
