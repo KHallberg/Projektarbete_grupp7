80f78c76b7b673453bcd91ea5a635c90a21b93ce
nicolinao <anilocin@hotmail.com>
2016-03-17 15:35:12 +0100

    Mer pill

--- Calender/src/KalenderGUI.java
+++ Calender/src/KalenderGUI.java
@@ -70,20 +70,20 @@ public class KalenderGUI extends JFrame {
 		JLabel lblHeadline = new JLabel("");
 		lblHeadline.setFont(new Font("Futura LT", Font.PLAIN, 39));
 		lblHeadline.setForeground(Color.BLACK);
-		lblHeadline.setBounds(85, 321, 800, 50);
+		lblHeadline.setBounds(65, 324, 800, 50);
 		contentPane.add(lblHeadline);
 		lblHeadline.setText(a.getInfo().get(0).getTitle().toString());
 		
 		JLabel lblTime_1 = new JLabel("");
 		lblTime_1.setFont(new Font("Futura LT", Font.PLAIN, 20));
 		lblTime_1.setForeground(Color.BLACK);
-		lblTime_1.setBounds(85, 409, 200, 16);
+		lblTime_1.setBounds(129, 409, 200, 16);
 		contentPane.add(lblTime_1);
 		lblTime_1.setText(a.getInfo().get(0).getTimeStart().toString()+"-"+a.getInfo().get(0).getTimeEnd().toString());
 		
 		JLabel lblPlace = new JLabel("PLACE");
 		lblPlace.setFont(new Font("Futura LT", Font.PLAIN, 20));
-		lblPlace.setBounds(85, 438, 650, 20);
+		lblPlace.setBounds(129, 439, 607, 20);
 		contentPane.add(lblPlace);
 		lblPlace.setText(a.getInfo().get(0).getPlace().toString());
 		
@@ -91,7 +91,7 @@ public class KalenderGUI extends JFrame {
 		txtpnDetails.setEditable(false);
 		txtpnDetails.setBackground(new Color(240, 241, 241));
 		txtpnDetails.setFont(new Font("Futura LT", Font.PLAIN, 22));
-		txtpnDetails.setBounds(105, 518, 484, 340);
+		txtpnDetails.setBounds(105, 518, 988, 340);
 		contentPane.add(txtpnDetails);
 		txtpnDetails.setText(a.getInfo().get(0).getDescription().toString());
 		
@@ -119,11 +119,33 @@ public class KalenderGUI extends JFrame {
 		contentPane.add(lblTime_3);
 		lblTime_3.setText(a.getInfo().get(1).getTimeStart().toString()+"-"+a.getInfo().get(1).getTimeEnd().toString());
 		
+		JLabel lblTid = new JLabel("Tid:");
+		lblTid.setFont(new Font("Futura LT", Font.BOLD, 20));
+		lblTid.setBounds(65, 407, 53, 20);
+		contentPane.add(lblTid);
+		
+		JLabel lblPlats = new JLabel("Plats:");
+		lblPlats.setFont(new Font("Futura LT", Font.BOLD, 20));
+		lblPlats.setBounds(65, 440, 71, 18);
+		contentPane.add(lblPlats);
+		
+		JLabel lblMars = new JLabel("21 MARS:");
+		lblMars.setForeground(new Color(255, 255, 255));
+		lblMars.setFont(new Font("Futura LT", Font.PLAIN, 40));
+		lblMars.setBounds(1281, 690, 209, 50);
+		contentPane.add(lblMars);
+		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setFont(new Font("Futura LT", Font.PLAIN, 13));
 		lblNewLabel.setIcon(new ImageIcon(KalenderGUI.class.getResource("/GUIBackground/Kalendersk\u00E4rm_clean.png")));
 		lblNewLabel.setBounds(0, 0, (int)width, (int)height);
 		contentPane.add(lblNewLabel);
+		
+
+		
+
+		
+
 		
 		
 		
