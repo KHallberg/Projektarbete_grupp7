3a3faa45abc3be3f4b6ff554611e7d4a55c2235e
Andrée Rehnberg <andree.rehnberg@gmail.com>
2016-03-21 12:10:07 +0100

    moddar skånetrafiken API

--- Calender/src/se/mah/k3lara/skaneAPI/control/Constants.java
+++ Calender/src/se/mah/k3lara/skaneAPI/control/Constants.java
@@ -4,8 +4,9 @@ import se.mah.k3lara.skaneAPI.model.Station;
 
 
 public class Constants {
-	public static final String testURL = "http://www.labs.skanetrafiken.se/v2.2/resultspage.asp?cmdaction=next&selPointFr=%7C81116%7C0&selPointTo=%7C65008%7C0&LastStart=2012-10-14%2008:00";
-	public static final String baseURL = "http://www.labs.skanetrafiken.se/v2.2/";
+	public static final String testURL = "https://mah.se/Nyheter/RSS/Kalender-fran-Malmo-hogskola/";
+	public static final String baseURL = "https://mah.se/Nyheter/RSS/Kalender-fran-Malmo-hogskola/";
+	/*
 	public static final String queryURL = "resultspage.asp?cmdaction=next&selPointFr=";
 	public static final String getStationURL = "querystation.asp?inpPointfr=";
 	public static final String stationResultURL = "stationresults.asp?selPointFrKey=";
@@ -15,6 +16,7 @@ public class Constants {
 	public static final String lastPartURL = "0&LastStart=";
 	public static final String noOfResults = "&NoOf=";
 	public static int nbrResultsToGet = 25;
+	*/
 
 
 	/**
@@ -25,6 +27,8 @@ public class Constants {
 	 * @param time in format hh:mm 24 H
 	 * @param nbrResults max 20
 	 * */
+	
+	/*
 	public static String getURL(String startStationNumber, String endStationNumber  , String date, String time, int nbrResults){
 		String nbrRes = String.valueOf(nbrResults);
 		String url = baseURL + queryURL + pipe+startStationNumber + pipe + midPartURL +pipe + endStationNumber + pipe + lastPartURL + date + space + time+noOfResults+nbrRes;
@@ -37,12 +41,14 @@ public class Constants {
 	 * @param endStationNumber from skånetrafiken
 	 * @param nbrResults max 20 from now
 	 * */
+	
+	/*
 	public static String getURL(String startStationNumber, String endStationNumber, int nbrResults){
 		String nbrRes = String.valueOf(nbrResults);
 		String url = baseURL + queryURL + pipe+startStationNumber + pipe + midPartURL +pipe + endStationNumber + pipe + "0"+ noOfResults+nbrRes;
 		return url;
 	}
-	
+	*/
 	/**
 	 * Build the StationresultURL
 	 * @param startStationNumber from skånetrafiken
